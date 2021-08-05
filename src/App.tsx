883c0da55ef7ceb156b9b505cc7e85146bed3ca4
Noiri <suleipenn@gmail.com>
2021-08-05 12:01:31 +0900

    時間がずれるバグ&終了後に値がマイナスになるバグを修正しました.

--- src/App.tsx
+++ src/App.tsx
@@ -31,9 +31,19 @@ const App: FC = () => {
     }
   };
 
-  // 30分刻みのほうがいいかも
   const tick = (): void => {
     setRemainSec((t) => t - 1);
+    if (remainSec < 0) {
+      // 時間切れの場合：6時間後にセット
+      setRemainSec(
+        21600 - DateTime.local().minute * 60 - DateTime.local().second
+      );
+      setTargetTime({
+        hour: Math.floor((DateTime.local().hour + 6) % 24),
+        minute: DateTime.local().minute < 30 ? 30 : 0,
+        second: 0,
+      });
+    }
   };
 
   useEffect(() => {
--- src/SetTargetTimeButton.tsx
+++ src/SetTargetTimeButton.tsx
@@ -80,7 +80,9 @@ const SetTargetTimeButton: FC<Props> = ({ setTargetTime, setRemainSec }) => {
     );
     // console.log(localStorage.getItem("myTargetTime"));
     setRemainSec(
-      options[selectedIndex].diff(DateTime.local(), "second").seconds
+      Math.floor(
+        options[selectedIndex].diff(DateTime.local(), "second").seconds
+      )
     );
   };
 
@@ -99,7 +101,7 @@ const SetTargetTimeButton: FC<Props> = ({ setTargetTime, setRemainSec }) => {
     let { hour, minute } = DateTime.local();
 
     let tmpDate = DateTime.local();
-
+    tmpDate = tmpDate.set({ second: 0 });
     if (minute > 30) {
       minute = 1;
       hour = (hour + 1) % 24;
